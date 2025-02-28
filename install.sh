99d977ad2669e028aaf46cd766e8f0db48d9a2b3
Sandmann <31391404+sandman21vs@users.noreply.github.com>
2025-02-28 13:53:18 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 set -e
 
-# Atualiza a lista de pacotes no início
-echo "Atualizando a lista de pacotes..."
-apt update
-
 # Verifica se o script está sendo executado como root
 if [ "$EUID" -ne 0 ]; then
   echo "Este script precisa ser executado com privilégios de root."
   echo "Por favor, execute: sudo bash <(curl -fsSL https://raw.githubusercontent.com/sandman21vs/linux_setup/main/install.sh)"
   exit 1
-fi
-
-# Forçar a criação do grupo 'docker', se não existir
-if ! getent group docker > /dev/null 2>&1; then
-  echo "Grupo docker não existe. Criando o grupo..."
-  groupadd docker
-else
-  echo "O grupo docker já existe."
 fi
 
 # Adiciona o usuário original (se existir) ao grupo docker, se ainda não estiver
@@ -28,8 +16,11 @@ if ! id -nG "$USER_TO_ADD" | grep -qw "docker"; then
   echo "Atenção: Faça logout e login novamente para que a alteração tenha efeito."
 fi
 
+echo "Atualizando a lista de pacotes..."
+apt update
+
 # Lista de pacotes a verificar/instalar
-packages=(openssh-server micro btop python3)
+packages=(openssh-server micro btop python3 tailscale)
 
 for pkg in "${packages[@]}"; do
     echo "Verificando o pacote $pkg..."
@@ -41,10 +32,6 @@ for pkg in "${packages[@]}"; do
         apt install -y "$pkg"
     fi
 done
-
-# Instalação do Tailscale
-echo "Instalando Tailscale..."
-curl -fsSL https://tailscale.com/install.sh | sh
 
 # Instalação do Docker
 if ! command -v docker &>/dev/null; then
