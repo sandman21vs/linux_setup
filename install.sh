c4cb0dc309d5f36a6948b64e3ebbae1663f13f55
Sandmann <31391404+sandman21vs@users.noreply.github.com>
2025-02-28 13:09:50 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ else
 fi
 
 docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
+docker run -d -p 8000:8000 -p 9000:9443 --name portainer \
   --restart always \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v portainer_data:/data \
